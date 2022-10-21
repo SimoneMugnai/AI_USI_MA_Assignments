Fixed error in calculation of range

DeJongFunctions/__init__.py
@@ -97,7 +97,7 @@ class DeJong:
         assert 6 > func_number > 0, 'func_number needs to be a number between 1 and 5'
         self.function_name, self.fun, min_, self.resolution_factor, self.dimension = self.funcs[func_number]
         self.digits_after = len(str(self.resolution_factor)[2:])
-        self.range = (-min_, min_ - self.resolution_factor)
+        self.range = (-min_, min_)
         self.num_bits = int(np.log2(int(2 * (min_ / self.resolution_factor))))
         if dimension_in:
             self.dimension = dimension_in